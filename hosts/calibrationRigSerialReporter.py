3eaa0ff1c275d68604bb8523860575b0eed643cd
Ethan Li <lietk12@gmail.com>
2017-12-01 16:01:10 -0800

    Added Serial logging message handling.

--- hosts/calibrationRigSerialReporter.py
+++ hosts/calibrationRigSerialReporter.py
@@ -6,6 +6,13 @@ import serial
 
 FILENAME = 'calibrationRig.csv'
 
+def get_episode_id(line):
+    episode_id = line[1:].split(',')[0]
+    if episode_id == 'episodeID':
+        return None
+    else:
+        return int(episode_id)
+
 def main():
     print('Please connect the calibration rig now...')
     time.sleep(2.0)
@@ -23,19 +30,30 @@ def main():
 
     try:
         num_fields_expected = None
+        current_episode = None
         while True:
             line = str(ser.readline(), 'ascii').rstrip()
             num_fields = line.count(',')
             if num_fields_expected is None:
                 num_fields_expected = num_fields
                 print('Number of fields in output:', num_fields_expected)
-            if not (line.startswith('[') and line.endswith(']') and
-                    num_fields == num_fields_expected):
-                print('Malformed line:', line)
-            else:
+            if line.startswith('[') and line.endswith(']') and num_fields == num_fields_expected:
                 print(line[1:-1], file=f)
                 f.close()
+                new_episode = get_episode_id(line)
+                if (new_episode != current_episode) and (new_episode is not None):
+                    current_episode = new_episode
+                    if current_episode % 10 == 0:
+                        print('Starting episode {}...'.format(current_episode))
                 f = open('calibrationRig.csv', 'a')
+            elif current_episode is not None and line.startswith('E: '):
+                print('Error on episode {}: {}'.format(current_episode, line[2:]))
+            elif current_episode is not None and line.startswith('W: '):
+                print('Warning on episode {}: {}'.format(current_episode, line[2:]))
+            elif current_episode is None:
+                print('Initialization message: {}'.format(line))
+            else:
+                print('Malformed line:', line)
     except KeyboardInterrupt:
         print('Quitting...')
         f.close()
