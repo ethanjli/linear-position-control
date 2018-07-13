5ec34371b059d82c6989f34a97256672ae04ba8e
Ethan Li <lietk12@gmail.com>
2018-07-13 00:20:20 -0700

    Updated sensor smoothing test sketch.

--- tests/sensorSmoothing/sensorSmoothing.ino
+++ tests/sensorSmoothing/sensorSmoothing.ino
@@ -1,11 +1,11 @@
-#include <AnalogSensor.h>
-#include <Smoothing.h>
+#define LPC_Components_AnalogSensor
+#define LPC_Signal_Smoothing
+#include <LinearPositionControl.h>
 
 using namespace LinearPositionControl;
-// Globals
 
 Components::AnalogSensor potentiometer(A0);
-DiscreteSmoother smoother(potentiometer.state, 0.01, 1023, true, 2.0);
+Signal::DiscreteSmoother smoother(potentiometer.state, 0.01, 1023, true, 2.0);
 
 void setup() {
   Serial.begin(115200);
@@ -18,9 +18,9 @@ void loop() {
   potentiometer.update();
   smoother.update();
   Serial.print("0,1023,");
-  Serial.print(potentiometer.state.current());
+  Serial.print(potentiometer.state.current);
   Serial.print(',');
-  Serial.print(smoother.output.current());
+  Serial.print(smoother.output.current);
   Serial.println();
 }
 
